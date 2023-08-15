97ae6c47358b0cc5d08ee2a741891e640a702d0c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-15 19:29:11 +0200

    gig : fix sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,39 @@
-import { useState, /*CSSProperties*/ } from "react";
+import { useState /*CSSProperties*/ } from "react";
 import { GetRandomColor } from "./Helpers";
 // import Badge from "react-bootstrap/Badge"
 import { marked } from "marked";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function App() {
-  const [textColor] = useState(GetRandomColor())
-  const [markdown, setMarkdown] = useState('')
+  const [textColor] = useState(GetRandomColor());
+  const [markdown, setMarkdown] = useState("");
 
   return (
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/mdp/"}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div className="container-row">
-                <span style={{ color: textColor }}>Hello</span>
-                &nbsp;<span style={{ color: textColor }}>World</span>
-              </div>
+    <>
+      <div className="container-hello" style={{ userSelect: 'none'}}>
+        <span style={{ color: textColor }}>Hello</span>
+        &nbsp;<span style={{ color: textColor }}>World</span>
+      </div>
 
-              <div className="mdp-body">
-                <div className="left">
-                  <textarea className="input"
-                    value={markdown}
-                    onChange={(e) => {
-                      setMarkdown(e.target.value);
-                    }}
-                    placeholder="👯 Write Your Markdown here 👯"
-                  ></textarea>
-                </div>
-                <div className="right">
-                  <div className='output'
-                    dangerouslySetInnerHTML={{ __html: marked(markdown),}}
-                  ></div>
-                </div>
-              </div>
-            </>
-          }
-        />
-        <Route path="/about" element={<div>About</div>} />
-      </Routes>
-    </BrowserRouter>
+      <div className="mdp-body">
+        <div className="left">
+          <textarea
+            className="input"
+            value={markdown}
+            onChange={(e) => {
+              setMarkdown(e.target.value);
+            }}
+            placeholder="👯 Write Your Markdown here 👯"
+          ></textarea>
+        </div>
+        <div className="right">
+          <div
+            className="output"
+            dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+          ></div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
