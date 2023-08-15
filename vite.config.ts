809b860750f62f8a7d47a1a7df1153218f94aee0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-15 17:23:33 +0200

    MDP : fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["marked"],
-  },
 })
+*/
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    // baseurl: ''
+    build: {
+      assetsInclude: ['src/styles/styles.css'],
+    }
+  }
+
+  if (command !== 'serve') {
+    config.base = '/coughing/'
+  }
+
+  return config
+})
+
