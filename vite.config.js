d093e2e453c688abc4d54415e0ebe9febd171e19
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2024-03-08 00:54:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
   }
 
   if (command !== 'serve') {
-    config.base = '/mdp/'
+    config.base = '/interview_react_confirmation_component/'
   }
 
   return config
